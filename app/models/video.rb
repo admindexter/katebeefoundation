97845cbcfa9287a492ab24445f61b1478ad02ec9
Dextersmart Harry <dextersmart@gmail.com>
2010-07-19 16:50:08 +0100

    Commit after modifying models

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,8 +3,6 @@ class Video < ActiveRecord::Base
 
   acts_as_textiled :information
 
-  has_attached_file :photo
-
   validates_uniqueness_of :title
   validates_presence_of :title, :information
 end
